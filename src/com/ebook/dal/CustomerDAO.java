37ca2ce4b993213502c44e693911a2b96801123c
Julia <julia.cicale@gmail.com>
2017-09-26 16:03:40 -0500

    added addCustomer to CustomerDAO

--- src/com/ebook/dal/CustomerDAO.java
+++ src/com/ebook/dal/CustomerDAO.java
@@ -13,7 +13,7 @@ public class CustomerDAO {
 	
 	public Customer getCustomer(String customerId) {		
 		  try { 		
-	    	//Get Customer
+	    	//Get Customer - READ Operation
 	    	Statement st = DBHelper.getConnection().createStatement();
 	    	String selectCustomerQuery = "SELECT customer_id, last_name, first_name, username, password "
 	    			+ "FROM Customer WHERE customer_id = '" + customerId + "'";
@@ -21,21 +21,21 @@ public class CustomerDAO {
 	    	ResultSet custRS = st.executeQuery(selectCustomerQuery);      
 	    	System.out.println("CustomerDAO: *************** Query " + selectCustomerQuery);
 	    	
-	      //Get Customer
-    	  Customer customer = new Customer();
-	      while ( custRS.next() ) {
+	    //Get Customer
+	    	Customer customer = new Customer();
+	    while ( custRS.next() ) {
 	    	  customer.setCustomerId(custRS.getString("customer_id"));
 	    	  customer.setLastName(custRS.getString("last_name"));
 	    	  customer.setFirstName(custRS.getString("first_name"));
 	    	  customer.setUserName(custRS.getString("username"));
 	    	  customer.setPassword(custRS.getString("password"));
-	      }
-	      //close to manage resources
-	      custRS.close();
-	     //Get Billing Address
-	      String selectBillingAddress = "SELECT customer_id, address_id, type, status FROM CustomerAddresses WHERE customer_id = '" + customerId + "' AND type = '1' AND status = 'Active'";
-	      ResultSet addRS = st.executeQuery(selectBillingAddress);
-	      if(addRS.next()) {
+	    }
+	    //close to manage resources
+	    custRS.close();
+	    //Get Billing Address
+	    String selectBillingAddress = "SELECT customer_id, address_id, type, status FROM CustomerAddresses WHERE customer_id = '" + customerId + "' AND type = '1' AND status = 'Active'";
+	    ResultSet addRS = st.executeQuery(selectBillingAddress);
+	    if(addRS.next()) {
 	    	  
 	    	  Address billingAddress =new Address();
 	    	  billingAddress.setAddressId(addRS.getInt("address_id"));
@@ -106,38 +106,83 @@ public class CustomerDAO {
 	  }
 	
 	public void addCustomer(Customer cust) {
-		/*Connection con = DBHelper.getConnection();
+		//Add Customer - CREATE Operation
+		Connection con = DBHelper.getConnection();
         PreparedStatement custPst = null;
         PreparedStatement addPst = null;
-
+        PreparedStatement addShipPst = null;
+        PreparedStatement custAddPst = null;
+        PreparedStatement custAddShipPst = null;
+        
         try {
-        	//Insert the customer object
-            String custStm = "INSERT INTO Customer(customerID, lname, fname) VALUES(?, ?, ?)";
+        	    //Insert the customer object
+            String custStm = "INSERT INTO Customer(customer_id, last_name, first_name, username, password) VALUES(?, ?, ?, ?, ?)";
             custPst = con.prepareStatement(custStm);
             custPst.setString(1, cust.getCustomerId());
             custPst.setString(2, cust.getLastName());       
-            custPst.setString(3, cust.getFirstName()); 
+            custPst.setString(3, cust.getFirstName());
+            custPst.setString(4, cust.getUserName());
+            custPst.setString(5, cust.getPassword());
             custPst.executeUpdate();
+            System.out.println("Added to Customer Table...");
 
-        	//Insert the customer address object
-            String addStm = "INSERT INTO Address(customerID, addressID, street, unit, city, state, zip) VALUES(?, ?, ?, ?, ?, ?, ?)";
+        	    //Insert the billing address object
+            String addStm = "INSERT INTO Address(address_id, street, unit, city, state, zip) VALUES(?, ?, ?, ?, ?, ?)";
             addPst = con.prepareStatement(addStm);
-            addPst.setString(1, cust.getCustomerId());
-            addPst.setString(2, cust.getBillingAddress().getAddressId());  
-            addPst.setString(3, cust.getBillingAddress().getStreet());       
-            addPst.setString(4, cust.getBillingAddress().getUnit());  
-            addPst.setString(5, cust.getBillingAddress().getCity());  
-            addPst.setString(6, cust.getBillingAddress().getState());      
-            addPst.setString(7, cust.getBillingAddress().getZip());  
+            addPst.setInt(1, cust.getBillingAddress().getAddressId());  
+            addPst.setString(2, cust.getBillingAddress().getStreet());       
+            addPst.setString(3, cust.getBillingAddress().getUnit());  
+            addPst.setString(4, cust.getBillingAddress().getCity());  
+            addPst.setString(5, cust.getBillingAddress().getState());      
+            addPst.setString(6, cust.getBillingAddress().getZip());  
             addPst.executeUpdate();
+            System.out.println("Added billing address to Address Table...");
+            
+            //Insert shipping address object
+            String addShipStm = "INSERT INTO Address(address_id, street, unit, city, state, zip) VALUES (?, ?, ?, ?, ?, ?)";
+            addShipPst = con.prepareStatement(addShipStm);
+            addShipPst.setInt(1, cust.getShippingAddress().getAddressId());  
+            addShipPst.setString(2, cust.getShippingAddress().getStreet());       
+            addShipPst.setString(3, cust.getShippingAddress().getUnit());  
+            addShipPst.setString(4, cust.getShippingAddress().getCity());  
+            addShipPst.setString(5, cust.getShippingAddress().getState());      
+            addShipPst.setString(6, cust.getShippingAddress().getZip());  
+            addShipPst.executeUpdate();
+            System.out.println("Added shipping address to Address Table...");
+            
+            //Insert Customer Addresses table
+            String custAddStm = "INSERT INTO CustomerAddresses(customer_id, address_id, type, status) VALUES(?, ?, ?, ?)";
+            custAddPst = con.prepareStatement(custAddStm);
+            custAddPst.setString(1, cust.getCustomerId());
+            custAddPst.setInt(2, cust.getBillingAddress().getAddressId());
+            custAddPst.setInt(3, 1);
+            custAddPst.setString(4, "Active");
+            custAddPst.executeUpdate();
+            System.out.println("Added billing address to CustomerAddresses Table...");
+            
+            String custAddShipStm = "INSERT INTO CustomerAddresses(customer_id, address_id, type, status) VALUES(?, ?, ?, ?)";
+            custAddShipPst = con.prepareStatement(custAddShipStm);
+            custAddShipPst.setString(1, cust.getCustomerId());
+            custAddShipPst.setInt(2, cust.getShippingAddress().getAddressId());
+            custAddShipPst.setInt(3, 2);
+            custAddShipPst.setString(4, "Active");
+            custAddShipPst.executeUpdate();
+            System.out.println("Added shipping address to CustomerAddresses Table...");
+            
+            
         } catch (SQLException ex) {
-
+        		System.err.println("CustomerDAO: Threw a SQLException saving the customer object.");
+        		System.err.println(ex.getMessage());
         } finally {
 
             try {
                 if (addPst != null) {
                 	addPst.close();
                 	custPst.close();
+                	addShipPst.close();
+                	custAddPst.close();
+                	custAddShipPst.close();
+                	
                 }
                 if (con != null) {
                     con.close();
@@ -145,8 +190,8 @@ public class CustomerDAO {
 
             } catch (SQLException ex) {
       	      System.err.println("CustomerDAO: Threw a SQLException saving the customer object.");
-    	      System.err.println(ex.getMessage());
+      	      System.err.println(ex.getMessage());
             }
-        }*/
+        }
     }
 }
